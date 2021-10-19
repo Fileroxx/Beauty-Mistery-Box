e61c2721d7ba3b6452f97f8fd498bbbc5b80f8c4
Igor Fernandes <igor_cesari@hotmail.com>
2021-10-19 13:21:56 -0300

    BMB - Complete Deploy

---
+++ js/index.js
@@ -0,0 +1,28 @@
+AOS.init();
+
+
+// DESATIVAR CLIQUE COM O BOTÃO DIREITO
+
+// document.addEventListener('contextmenu', event => event.preventDefault());
+
+
+//DESATIVAR F12
+
+// document.onkeypress = function (event) {
+//     event = (event || window.event);
+//     if (event.keyCode == 123) {
+//         return false;
+//     }
+// }
+// document.onmousedown = function (event) {
+//     event = (event || window.event);
+//     if (event.keyCode == 123) {
+//         return false;
+//     }
+// }
+// document.onkeydown = function (event) {
+//     event = (event || window.event);
+//     if (event.keyCode == 123) {
+//         return false
+//     }
+// }
